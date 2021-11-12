a7e7e8c72726af643a8be8b5db8a23c288f0973b
Neelu Begum <34142655+NeeluBegum@users.noreply.github.com>
2021-11-12 16:46:01 +0000

    Update downstream_fungi.r

--- bin/downstream_fungi.r
+++ bin/downstream_fungi.r
@@ -63,42 +63,5 @@ names(genesizes) = sizeTab$gene_id
 #print('number of rows of gctdown10m')
 #print(nrow(gctdown10m))
 gctNorm10m = momr::normFreqRPKM(dat=gctdown10m, cat=genesizes)
-#write.csv(gctNorm10m, quote=F, file="norm.csv")
+write.csv(gctNorm10m, quote=F, file="norm.csv")
 print("norm finished")
-
-
-
-
-
-
-#delete below
-print("catalog info loading")
-MSP_data = read.csv(mspdownload, sep="\t", stringsAsFactors=F, header=T)
-MSP_data[MSP_data==""] <- NA
-MSP_data <- MSP_data[!(is.na(MSP_data$msp_name)),]
-print("catalog info loaded")
-
-print("mgs generation begin")
-MSP_id = split(MSP_data$gene_id, MSP_data$msp_name)
-mgsList = MSP_id
-mgsList_MG <- mgsList 
-# only the first 50 genes - enough information with them
-for(i in 1:length(mgsList_MG)){
-	mgsList_MG[[i]] <- mgsList_MG[[i]][1:50]
-}
-mgsList = mgsList_MG
-mgsGeneList = unique(do.call(c, mgsList))
-genes_id <- sizeTab$gene_id[match(mgsGeneList, sizeTab$gene_id)]
-id <- match(genes_id, rownames(gctNorm10m))
-data <- gctNorm10m[id,]
-rownames(data) <- mgsGeneList
-data[is.na(data)] <- 0
-genebag = rownames(data)
-mgs <- momr::projectOntoMGS(genebag=genebag, list.mgs=mgsList)
-length(genebag)
-mgs.dat <- momr::extractProfiles(mgs, data)
-mgs.med.vect <- momr::computeFilteredVectors(profile=mgs.dat, type="median")
-mgs.med.vect <- mgs.med.vect[rowSums(mgs.med.vect)>0,]
-write.csv(mgs.med.vect, quote=F, file="msp.csv")
-
-print("fungal genecount table generation done")
